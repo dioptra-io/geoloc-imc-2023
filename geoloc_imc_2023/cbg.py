6c69d0572ef03b17ef9632e8642c980ac361baa2
hrimlinger <hugorimlinger4@gmail.com>
2023-04-25 17:38:34 +0200

    prepare probes measurements

--- geoloc_imc_2023/cbg.py
+++ geoloc_imc_2023/cbg.py
@@ -1,14 +1,15 @@
 import pickle
 import logging
 
+from pathlib import Path
 from random import randint, choice
 from collections import defaultdict
 
 from geoloc_imc_2023.atlas_probing import RIPEAtlas
 from geoloc_imc_2023.default import (
     NB_PACKETS,
-    HITLIST_ADDR_PATH,
-    ANCHOR_PATH,
+    NB_TARGETS_PER_PREFIX,
+    HITLIST_PATH,
 )
 
 logger = logging.getLogger()
@@ -25,52 +26,54 @@ class CBG():
     def __init__(
             self,
             ripe_credentials: dict[str, str],
-            hitlist_addr: dict,
-            anchors: dict,
-            nb_target_addr: int = 3,
-            nb_packets: int = 3,
         ) -> None:
 
-        self.anchors = anchors
-        self.hitlist_addr = hitlist_addr
         self.account = ripe_credentials["username"]
         self.key = ripe_credentials["key"]
-        self.nb_target_addr = nb_target_addr
-        self.nb_packets = nb_packets
 
-        self.driver = RIPEAtlas(self.account, self.key, self.anchors)
+        self.driver = RIPEAtlas(self.account, self.key)
 
-    def get_target_hitlist(self, target_prefix: str) -> list[str]:
+
+    def get_target_hitlist(self, target_prefix: str, nb_targets: int, targets_per_prefix: dict) -> list[str]:
         """from ip, return a list of target ips"""
         target_addr_list = []
         try:
-            target_addr_list = self.hitlist_addr[target_prefix]
+            target_addr_list = targets_per_prefix[target_prefix]
         except KeyError:
             pass
 
-        if len(target_addr_list) < self.nb_target_addr:
-            target_addr_list.extend([str(target_prefix[randint(1,254)]) for _ in range(0,self.nb_target_addr - len(target_addr_list))])
-        if len(target_addr_list) > self.nb_target_addr:
-            target_addr_list = target_addr_list[:self.nb_target_addr]
+        if len(target_addr_list) < nb_targets:
+            target_addr_list.extend([str(target_prefix[randint(1,254)]) for _ in range(0,nb_targets - len(target_addr_list))])
+        if len(target_addr_list) > nb_targets:
+            target_addr_list = target_addr_list[:nb_targets]
 
         return target_addr_list
 
-    def prefix_probing(self, target_prefixes: list, vps: list, nb_packets: int = NB_PACKETS) -> dict:
+    def prefix_probing(
+            self, 
+            target_prefixes: list,
+            vps: dict,
+            targets_per_prefix: dict,
+            nb_packets: int = NB_PACKETS, 
+            nb_targets: int = NB_TARGETS_PER_PREFIX,
+            dry_run: bool = False,
+        ) -> dict:
         """from a list of prefixes, start measurements for n target addrs in prefix"""
         measurement_ids = defaultdict(list)
-        dry_run = False
         for _, target_prefix in enumerate(target_prefixes):
 
             # get target_addr_list
-            target_addr_list = self.get_target_hitlist(target_prefix, self.nb_target_addr)
+            target_addr_list = self.get_target_hitlist(target_prefix, nb_targets, targets_per_prefix)
 
-            vp_ids = [self.anchors[vp_addr]['id'] for vp_addr in vps if vp_addr not in target_addr_list]
+            vp_ids = [vps[vp_addr]['id'] for vp_addr in vps if vp_addr not in target_addr_list]
 
-            logger.info(f"starting measurement for {target_prefix=} with {[addr for addr in target_addr_list]} with {len(vps)} self.anchors")    
+            logger.info(f"starting measurement for {target_prefix=} with {[addr for addr in target_addr_list]} with {len(vps)}")    
 
             for target_addr in target_addr_list:
                 if dry_run:
                     logger.debug(f"measurement for {target_addr}")
+                    measurement_id = 404
+                    measurement_ids[target_prefix].append(measurement_id)
                     continue
                 else:
                     measurement_id = self.driver.probe(str(target_addr), vp_ids, nb_packets)
@@ -78,68 +81,31 @@ class CBG():
         
         return measurement_ids
 
-    def target_probing(self, targets: list, vps: list, nb_packets: int = NB_PACKETS) -> dict:
+    def target_probing(
+            self,
+            targets: list,
+            vps: dict,
+            nb_packets: int = NB_PACKETS,
+            dry_run: bool = False
+        ) -> dict:
         """from a list of prefixes, start measurements for n target addrs in prefix"""
         measurement_ids = defaultdict(list)
-        dry_run = False
         for _, target_addr in enumerate(targets):
 
             # get target_addr_list
-            vp_ids = [self.anchors[vp_addr]['id'] for vp_addr in vps if vp_addr != target_addr]
+            vp_ids = [vps[vp_addr]['id'] for vp_addr in vps if vp_addr != target_addr]
 
             logger.info(f"starting measurement for {target_addr=} with {len(vps)} self.anchors")    
 
-            for target_addr in target_addr:
-                if dry_run:
-                    logger.debug(f"measurement for {target_addr}")
-                    continue
-                else:
-                    measurement_id = self.driver.probe(str(target_addr), vp_ids, nb_packets)
-                    measurement_ids[target_prefix].append(measurement_id)
+            if dry_run:
+                measurement_id = 404
+                measurement_ids[target_addr].append(measurement_id)
+                continue
+            else:
+                measurement_id = self.driver.probe(str(target_addr), vp_ids, nb_packets)
+                measurement_ids[target_addr].append(measurement_id)
         
-        return measurement_ids
-
-
-if __name__ == "__main__":
-
-    logging.basicConfig(level=logging.DEBUG)
-
-    NB_TARGET = 10
-    NB_VP = 10
-
-    ripe_credentials = {
-        "username": "timur.friedman@sorbonne-universite.fr",
-        "key": "b3d3d4fc-724e-4505-befe-1ad16a70dc87",
-    }
-
-    # load anchors
-    with open(ANCHOR_PATH, "rb") as f:
-        anchors = pickle.load(f)
-    
-    with open(HITLIST_ADDR_PATH, "rb") as f:
-        hitlist_addr = pickle.load(f)
-
-    targets= [choice(list(anchors)) for _ in range(0, NB_TARGET)]
-    vps = list(set(anchors).difference(set(targets)))[:NB_VP]
-
-    logging.info(f"nb targets: {len(targets)} : {[target for target in targets]}")
-    logging.info(f"nb_vps : {len(vps)} {[vp for vp in vps]} ")
-
-    cbg = CBG(targets, vps, ripe_credentials, hitlist_addr, anchors)
-
-    # get target prefixes
-    target_prefixes = []
-    for target_addr in targets:
-        target_prefix = get_prefix_from_ip(target_addr)
-        target_prefixes.append(target_prefix)
-    
-    # initialization: start measurements from all vps to targets
-    init_measurement_ids = cbg.start_init_measurements(target_prefixes)
-
-    # retreive measurement results from RIPE Atlas
-    logging.info(init_measurement_ids)
-
-    
+        return measurement_ids    
 
     
     
--- geoloc_imc_2023/default.py
+++ geoloc_imc_2023/default.py
@@ -3,10 +3,13 @@ from pathlib import Path
 
 
 # Path 
-HITLIST_ADDR_PATH : Path = Path(".") / "../datasets/parsed_hitlist.pickle"
-ANCHOR_PATH : Path = Path(".") / "../datasets/anchors.pickle"
+HITLIST_PATH : Path = Path().absolute() / "../datasets/parsed_hitlist.pickle"
+ANCHORS_PATH : Path = Path().absolute() / "../datasets/anchors.pickle"
+PROBES_PATH : Path = Path().absolute() / "../datasets/probes.pickle"
+RESULTS_PATH: Path = Path().absolute() / "../results/"
 
 # constant
 MAX_NUMBER_OF_VPS = 5_00
 NB_PACKETS = 3
+NB_TARGETS_PER_PREFIX = 3
 
