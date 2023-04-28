aebe0f17423830fabf6497928e35fa3ab8ee0990
hrimlinger <hugorimlinger4@gmail.com>
2023-04-28 12:01:20 +0200

    measurement scripts

---
+++ scripts/anchor_target_anchor_vp.py
@@ -0,0 +1,94 @@
+"""measure atlas anchor targets from atlas probes"""
+import logging
+import sys
+import uuid
+import argparse
+
+from geoloc_imc_2023.cbg import CBG
+from geoloc_imc_2023.default import LOG_PATH, RIPE_CREDENTIALS
+from geoloc_imc_2023.measurement_utils import (
+    load_atlas_anchors,
+    save_config_file,
+)
+
+
+if __name__ == "__main__":
+    # parse cmd line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dry_run",
+        help="boolean, decide wether or not to start actual probing, set to True for testing",
+        type=bool,
+    )
+    parser.add_argument(
+        "--nb_targets", help="define the number of targets to measure", type=int
+    )
+    parser.add_argument(
+        "--nb_vps", help="define the number of atlas vantage points to use", type=int
+    )
+    args = parser.parse_args()
+
+    dry_run = args.dry_run
+    nb_targets = args.nb_targets
+    nb_vps = args.nb_vps
+
+    # generate measurement UUID
+    measurement_uuid = uuid.uuid4()
+
+    # set logging utilities
+    logging.basicConfig(
+        filename=LOG_PATH / (str(measurement_uuid) + ".log"),
+        filemode="a",
+        format="%(asctime)s::%(name)s:%(levelname)s:%(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG,
+    )
+
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+    logger = logging.getLogger()
+
+    # load dataset
+    anchors = load_atlas_anchors()
+
+    # select targets and vps from anchors
+    targets = list(anchors.keys())[:nb_targets]
+    vps = {}
+    for i, anchor in enumerate(anchors):
+        if i >= nb_vps:
+            break
+        vps[anchor] = anchors[anchor]
+
+    logger.info(f"nb targets: {len(targets)}")
+    logger.info(f"nb_vps : {len(vps)}")
+
+    # measurement configuration for retrieval
+    measurement_config = {
+        "UUID": str(measurement_uuid),
+        "is_dry_run": dry_run,
+        "description": "measurement towards anchors with anchors as vps",
+        "type": "target",
+        "af": 4,
+        "targets": targets,
+        "vps": vps,
+    }
+
+    # save measurement configuration before starting measurement
+    save_config_file(measurement_config)
+
+    cbg = CBG(RIPE_CREDENTIALS)
+
+    logger.info(
+        f"Starting measurements {measurement_uuid} with parameters: {dry_run=} , nb_targets={len(nb_targets)}; nb_vps{len(nb_vps)}"
+    )
+
+    # measurement for 3 targets in every target prefixes
+    (
+        measurement_config["ids"],
+        measurement_config["start_time"],
+        measurement_config["end_time"],
+    ) = cbg.target_probing(
+        targets=targets, vps=vps, tag=measurement_uuid, dry_run=dry_run
+    )
+
+    save_config_file(measurement_config)
